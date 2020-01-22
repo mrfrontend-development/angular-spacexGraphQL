990b8e4caf6b12aee54f537fa255d4d964fc9fde
Raymon Schouwenaar <raymon@raymonschouwenaar.nl>
2020-01-22 11:35:56 +0100

    6: Show rocket data

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core'
+import { RouterModule, Routes } from '@angular/router'
+import { RocketsComponent } from './rockets/rockets.component'
 
-
-const routes: Routes = [];
+const routes: Routes = [
+	{
+		path: '',
+		component: RocketsComponent,
+	},
+]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/rockets/rockets.component.ts
+++ src/app/rockets/rockets.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { Subject } from 'rxjs'
+import { GetRocketsGQL, Rocket } from '../../generated/graphql'
 
 @Component({
-  selector: 'app-rockets',
-  templateUrl: './rockets.component.html',
-  styleUrls: ['./rockets.component.sass']
+	selector: 'app-rockets',
+	templateUrl: './rockets.component.html',
+	styleUrls: ['./rockets.component.sass'],
 })
 export class RocketsComponent implements OnInit {
+	protected destroy = new Subject<void>()
+	public rockets: Rocket[] = null
 
-  constructor() { }
+	constructor(private rocketsGQL: GetRocketsGQL) {}
 
-  ngOnInit() {
-  }
-
+	ngOnInit() {
+		this.rocketsGQL.watch().valueChanges.subscribe((result) => {
+			this.rockets = result.data.rockets
+		})
+	}
 }
