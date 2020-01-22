a9ac0df2c3b11e95197889dc01ded3861d5e0ab2
Raymon Schouwenaar <raymon@raymonschouwenaar.nl>
2020-01-22 10:47:15 +0100

    5: Create rockets component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GraphQLModule } from './graphql.module';
 import { HttpClientModule } from '@angular/common/http';
+import { RocketsComponent } from './rockets/rockets.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RocketsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/rockets/rockets.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rockets',
+  templateUrl: './rockets.component.html',
+  styleUrls: ['./rockets.component.sass']
+})
+export class RocketsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
